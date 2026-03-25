d10df269a4646ba96f3ef89b650036b421a285af
Chd00009 <chd00009@mix.wvu.edu>
2026-03-24 21:46:36 -0400

    Update query4_abc_classification.sql

--- sql/query4_abc_classification.sql
+++ sql/query4_abc_classification.sql
@@ -5,3 +5,14 @@
 -- 1. Calculate total revenue per product.
 -- 2. Compute cumulative revenue and percent of total revenue using window functions.
 -- 3. Assign ABC class: A (top 80%), B (next 15%), C (bottom 5%).
+
+-- 1:
+WITH product_revenue AS (
+    SELECT
+        oi.product_id,
+        p.product_category_name,
+        SUM(oi.price) AS total_revenue
+    FROM order_items oi
+    JOIN products p USING(product_id)
+    GROUP BY oi.product_id, p.product_category_name
+),
