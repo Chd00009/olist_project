43b082463cd777cd786e630d307f7fd0f3123d81
Chd00009 <chd00009@mix.wvu.edu>
2026-03-24 21:49:20 -0400

    Update query3_seller_scorecard.sql

--- sql/query3_seller_scorecard.sql
+++ sql/query3_seller_scorecard.sql
@@ -25,3 +25,37 @@ WITH order_locations AS (
            ON c.customer_zip_code_prefix = g.geolocation_zip_code_prefix
     WHERE o.order_delivered_customer_date IS NOT NULL
 ),
+
+-- 2:
+delivery_times AS (
+    SELECT
+        order_id,
+        customer_state,
+        DATEDIFF(
+            'day',
+            order_estimated_delivery_date,
+            order_delivered_customer_date
+        ) AS delivery_delay
+    FROM order_locations
+),
+-- 3:
+region_summary AS (
+    SELECT
+        customer_state,
+        COUNT(*) AS total_orders,
+        AVG(delivery_delay) AS avg_delay,
+        MIN(delivery_delay) AS min_delay,
+        MAX(delivery_delay) AS max_delay
+    FROM delivery_times
+    GROUP BY customer_state
+)
+-- 4: Rank states by average delay:
+SELECT
+    customer_state,
+    total_orders,
+    ROUND(avg_delay, 2) AS avg_delay,
+    min_delay,
+    max_delay,
+    RANK() OVER (ORDER BY avg_delay DESC) AS state_rank
+FROM region_summary
+ORDER BY state_rank;
