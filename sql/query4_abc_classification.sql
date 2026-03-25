a4ccefe56837185e34d3013477146f8052b5b63d
Chd00009 <chd00009@mix.wvu.edu>
2026-03-24 21:49:54 -0400

    Update query4_abc_classification.sql

--- sql/query4_abc_classification.sql
+++ sql/query4_abc_classification.sql
@@ -16,3 +16,35 @@ WITH product_revenue AS (
     JOIN products p USING(product_id)
     GROUP BY oi.product_id, p.product_category_name
 ),
+
+-- 2:
+revenue_cumsum AS (
+    SELECT
+        product_id,
+        product_category_name,
+        total_revenue,
+        SUM(total_revenue) OVER (ORDER BY total_revenue DESC ROWS UNBOUNDED PRECEDING) AS cum_revenue,
+        SUM(total_revenue) OVER () AS total_revenue_all,
+        SUM(total_revenue) OVER (ORDER BY total_revenue DESC ROWS UNBOUNDED PRECEDING) * 1.0 / SUM(total_revenue) OVER () AS cum_revenue_pct
+    FROM product_revenue
+),
+
+-- 3:
+product_classification AS (
+    SELECT
+        product_id,
+        product_category_name,
+        total_revenue,
+        cum_revenue,
+        ROUND(cum_revenue_pct * 100, 2) AS cum_revenue_pct,
+        CASE
+            WHEN cum_revenue_pct <= 0.8 THEN 'A'
+            WHEN cum_revenue_pct <= 0.95 THEN 'B'
+            ELSE 'C'
+        END AS abc_class
+    FROM revenue_cumsum
+)
+-- 4: Final Select
+SELECT *
+FROM product_classification
+ORDER BY abc_class, cum_revenue_pct DESC;
