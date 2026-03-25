726c193c836526866be4ee931754eb6615288ad3
Chd00009 <chd00009@mix.wvu.edu>
2026-03-24 21:45:02 -0400

    Update query2_customer_retention.sql

--- sql/query2_customer_retention.sql
+++ sql/query2_customer_retention.sql
@@ -7,3 +7,13 @@
 -- 3. Aggregate by cohort (first purchase month) and calculate the % of customers 
 --    who placed a second order within 30, 60, and 90 days.
 -- 4. Use multiple CTEs to build intermediate tables for clarity and maintainability.
+
+-- 1:
+WITH first_purchase AS (
+    SELECT 
+        customer_id,
+        MIN(order_purchase_timestamp) AS first_order_date,
+        DATE_TRUNC('month', MIN(order_purchase_timestamp)) AS cohort_month
+    FROM orders
+    GROUP BY customer_id
+),
