3858d59203461f5cdcd57dbe4a762815a7b4ed45
Chd00009 <chd00009@mix.wvu.edu>
2026-03-24 21:45:53 -0400

    Update query3_seller_scorecard.sql

--- sql/query3_seller_scorecard.sql
+++ sql/query3_seller_scorecard.sql
@@ -5,3 +5,23 @@
 -- 1. Join orders → customers → geolocation for regional info.
 -- 2. Calculate delivery delays per order.
 -- 3. Aggregate delays per state and rank states by average delay.
+
+-- 1:
+WITH order_locations AS (
+    SELECT
+        o.order_id,
+        o.order_estimated_delivery_date,
+        o.order_delivered_customer_date,
+        c.customer_id,
+        c.customer_city,
+        c.customer_state,
+        g.geolocation_lat,
+        g.geolocation_lng,
+        g.geolocation_city,
+        g.geolocation_state
+    FROM orders o
+    JOIN customers c USING(customer_id)
+    LEFT JOIN geolocation g
+           ON c.customer_zip_code_prefix = g.geolocation_zip_code_prefix
+    WHERE o.order_delivered_customer_date IS NOT NULL
+),
